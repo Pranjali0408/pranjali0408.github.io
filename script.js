b804e9a7c310b3c5cd8776a970abfe4682263325
Pranjali0408 <138008174+Pranjali0408@users.noreply.github.com>
2026-03-29 22:13:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Optional JavaScript: Scroll effect (smooth navigation)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
