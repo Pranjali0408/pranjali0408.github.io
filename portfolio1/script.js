236ba7a5d07c48d4f0899d5687c95036c1031d4c
Pranjali0408 <138008174+Pranjali0408@users.noreply.github.com>
2026-03-29 20:43:50 +0530

    Add files via upload

---
+++ portfolio1/script.js
@@ -0,0 +1,9 @@
+// Optional JavaScript: Scroll effect (smooth navigation)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
